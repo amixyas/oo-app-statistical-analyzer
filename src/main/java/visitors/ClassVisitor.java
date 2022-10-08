040d6660e749691ccfeec61a53490814cd64257e
ilyvs <i.amirat@esi-sba.dz>
2022-10-08 21:00:11 +0200

    create Class-Methods visitor relation

--- src/main/java/visitors/ClassVisitor.java
+++ src/main/java/visitors/ClassVisitor.java
@@ -1,4 +1,31 @@
 package visitors;
 
-public class ClassVisitor {
+import org.eclipse.jdt.core.dom.*;
+
+import java.util.*;
+
+/** TODO :
+ * Visiting Classes
+ * Visiting Attributes
+ * Make next relations : Class-Attributes and Class-Methods
+*/
+
+public class ClassVisitor extends ASTVisitor{
+
+    /**  TODO : SOL-Variables-1
+     public static Map<String, FieldDeclaration[]> variables = new HashMap<String, FieldDeclaration[]>();
+     */
+
+    public static Map<String, MethodDeclaration[]> methods = new HashMap<>();
+    public  Map<String, MethodDeclaration[]> getMethods() {return methods;}
+
+    public boolean visit(TypeDeclaration node) {
+        if (!node.isInterface()) {
+            methods.put(node.getName().toString(), node.getMethods());
+        }
+        return super.visit(node);
+    }
+
 }
+
+
