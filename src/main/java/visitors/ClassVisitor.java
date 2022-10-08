c19662bb0da5f577b94f1e6389af2a191bfe2250
ilyvs <i.amirat@esi-sba.dz>
2022-10-08 21:02:35 +0200

    create Class-Attributes visitor relation (SOL-1)

--- src/main/java/visitors/ClassVisitor.java
+++ src/main/java/visitors/ClassVisitor.java
@@ -22,9 +22,25 @@ public class ClassVisitor extends ASTVisitor{
     public boolean visit(TypeDeclaration node) {
         if (!node.isInterface()) {
             methods.put(node.getName().toString(), node.getMethods());
+
+            /** TODO : SOL-Variables-1
+             * The problem with [node.getFields] that there is a such cases like [public String firstName, lastName, city]
+             it will consider it as one variable, so we need to make string treatment plus eliminating java reserved work
+             like [static final int string ... etc].
+
+             * The good thing here is that we can always make a relation between the variable and his class parent 'directly'.
+
+             variables.put(node.getName().toString(), node.getFields());
+             */
         }
         return super.visit(node);
     }
+
+    /** TODO : SOL-Variables-1
+     public Map<String, FieldDeclaration[]> getVariables() {
+     return variables;
+     }
+     */
 
 }
 
