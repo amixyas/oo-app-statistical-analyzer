72ca54e6c46165d6925023924238667a99761164
ilyvs <i.amirat@esi-sba.dz>
2022-10-09 09:33:04 +0200

    question 7 response

--- src/main/java/responses/Question3_5_7.java
+++ src/main/java/responses/Question3_5_7.java
@@ -4,13 +4,13 @@ import visitors.ClassVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
-public class Question3_5 {
+public class Question3_5_7 {
 
     CompilationUnit parse;
     public static ClassVisitor classVisitor = new ClassVisitor();
     public static int totalMethodsNbr = 0;
 
-    public Question3_5(CompilationUnit parse){
+    public Question3_5_7(CompilationUnit parse){
         this.parse = parse;
         parse.accept(classVisitor);
     }
