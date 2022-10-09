98965a7fc4051c0e02592db92c9f48b5ce227afe
ilyvs <i.amirat@esi-sba.dz>
2022-10-09 09:31:58 +0200

    question 3 response

---
+++ src/main/java/responses/Question3.java
@@ -0,0 +1,32 @@
+package responses;
+
+import visitors.ClassVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+public class Question3 {
+
+    CompilationUnit parse;
+    public static ClassVisitor classVisitor = new ClassVisitor();
+    public static int totalMethodsNbr = 0;
+
+    public Question3(CompilationUnit parse){
+        this.parse = parse;
+        parse.accept(classVisitor);
+    }
+
+    public static int getTotalMethodsNbr(){
+        for (String key : classVisitor.getMethods().keySet())
+            totalMethodsNbr +=  classVisitor.getMethods().get(key).length;
+        for (var entry : classVisitor.getMethods().entrySet()) {
+            // System.out.println("key : "+entry.getKey()+"\n");
+            for (MethodDeclaration method : entry.getValue()) {
+                // System.out.println(method.getName().toString());
+            }
+        }
+        return totalMethodsNbr;
+    }
+
+
+
+}
