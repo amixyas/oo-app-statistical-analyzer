a89d1be6608054e7223d8328bb46b0d2220faa20
amixyas <i.amirat@esi-sba.dz>
2022-10-14 18:39:05 +0200

    update parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import GUI_Package.GUIManager;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jdt.core.dom.*;
 import responses.*;
@@ -11,31 +13,49 @@ public class Parser {
     public static final String projectPath = "/home/ilyas/Documents/#Others/Univ-montpellier/Tps-um/ERL/oo-app-statistical-analyzer/src/main/resources/Calculator";
     //public static final String projectPath = "/home/ilyas/Documents/#Others/Univ-montpellier/Tps-um/ERL/tp1-part1/src/main/resources/Project";
     public static final String projectSourcePath = projectPath + "/src";
+    public static final String jrePath = "/usr/lib/jvm/java-11-openjdk-amd64";
+
+    static Question1 question1 = null;
+    //Question2 question2 = null;
+    static Question3_5_7 question3_5_7 = null;
+    static Question4 question4 = null;
+    static Question6 question6 = new Question6();
+    static Question8To13 question8To13;
+    static Question14 question14 = new Question14();
 
     public static void main(String[] args) throws IOException {
 
-        Question1 question1 = null;
-        Question2 question2 = null;
-        Question3_5_7 question3_5_7 = null;
-        Question4 question4 = null;
-        Question6 question6 = new Question6();
-        Question8To13 question8To13;
-        Question14 question14 = new Question14();
+        Scanner sc= new Scanner(System.in);
 
+        /**
+        System.out.println("Before we start, please provide to system the next requirements : ");
+        System.out.println("The project path you want to work on it : ");
+        String projectPath = sc.next();
+        String projectSourcePath = projectPath + "/src";
+        System.out.println("JDK path : ");
+        String jrePath = sc.next();
+        */
 
         // read java files
         final File folder = new File(projectSourcePath);
         ArrayList<File> javaFiles = listJavaFilesForFolder(folder);
         for (File fileEntry : javaFiles) {
             String content = FileUtils.readFileToString(fileEntry);
-            CompilationUnit parse = Config.createOwnParse(content.toCharArray(), projectSourcePath);
-            question1 = new Question1(parse);
-            question2 = new Question2(parse);
-            question3_5_7 = new Question3_5_7(parse);
+            CompilationUnit parse = Config.createOwnParse(content.toCharArray(), projectSourcePath,jrePath);
+            question1 = new Question1(parse); // fill package visitor
+            //question2 = new Question2(parse);
+            question3_5_7 = new Question3_5_7(parse); // fill class visitor
             question4 = new Question4(parse);
         }
+        question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
+        menu();
+    }
 
-        System.out.println("Menu : ");
+    static void menu(){
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("\nMenu : ");
         System.out.println("1 : Number of classes in the application.");
         System.out.println("2 : Number of application code lines.");
         System.out.println("3 : Total number of methods in the application..");
@@ -48,61 +68,76 @@ public class Parser {
         System.out.println("10 : Classes which belong to the two preceding categories at the same time.");
         System.out.println("11 : Classes that have more than X methods (the value of X is given).");
         System.out.println("12 : The 10% of methods that have the largest number of lines of code (per class).");
-        System.out.println("13 : The maximum number of parameters compared to all methods of the app");
+        System.out.println("13 : The maximum number of method parameters compared to all methods of the app");
         System.out.println("14 : Methods call graph.");
+        System.out.println("0 : Exit.");
 
-        int x = 0;
-        if (x==1){
-            System.out.println("1 : " + question1.getTotalClassesNbr());
+        System.out.print("What do you choose : ");
+        int input = sc.nextInt();
+        
+        while(input > 14 || input < 0 ){
+            System.out.print("Wrong input, choose again : ");
+            input = sc.nextInt();
         }
-        else if (x==2){
-            System.out.println("2 : " + question2.getTotalLines());
+
+        if (input==0){
+            System.exit(0);
         }
-        else if (x==3){
-            System.out.println("3 : " + question3_5_7.getTotalMethodsNbr());
+        if (input==1){
+            System.out.println("");
+            System.out.println("\nTotal classes = " + question1.printClasses());
         }
-        else if (x==4){
-            System.out.println("4 : " + question4.getTotalPackagesNbr());
+        else if (input==2){
+            ///System.out.println("2 : " + question2.getTotalLines());
         }
-        else if (x==5){
+        else if (input==3){
+            System.out.println("\nTotal methods = " + question3_5_7.printMethods());
+        }
+        else if (input==4){
+            System.out.println("\nTotal packages = " + question4.getTotalPackagesNbr());
+        }
+        else if (input==5){
             Double MoyMethodsPerClass = (question3_5_7.getTotalMethodsNbr() * 1.00 ) / question1.getTotalClassesNbr();
-            System.out.println("5 : " + MoyMethodsPerClass);
+            System.out.println("\nAverage number of methods per class = " + MoyMethodsPerClass);
         }
-        else if (x==6){
-            System.out.println("6 : " + question6.getMethodMoyLines());
+        else if (input==6){
+            System.out.println("\nAverage number of lines of code per method : " + question6.getMethodMoyLines());
         }
-        else if (x==7){
+        else if (input==7){
             Double MoyAttributesPerClass = question3_5_7.getTotalVariablesNbr()* 1.00/question1.getTotalClassesNbr();
-            System.out.println("7 : " + MoyAttributesPerClass);
+            System.out.println("\nAverage number of attributes per class = " + MoyAttributesPerClass);
         }
-        else if (x==8){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
-            question8To13.getClasses10Methods();
+        else if (input==8){
+            System.out.println("\nThe 10% of classes that have the most methods are : ");
+            question8To13.getClasses10Methods(true);
         }
-        else if (x==9){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
-            question8To13.getClasses10Attirbutes();
-
+        else if (input==9){
+            System.out.println("\nThe 10% of classes that have the most attributes are : ");
+            question8To13.getClasses10Attirbutes(true);
         }
-        else if (x==10){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
+        else if (input==10){
+            System.out.println("\nClasses which belong to the two preceding categories (8 and 9) at the same time are: ");
             question8To13.getClasses10methodsAttributes();
         }
-        else if (x==11){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
-            question8To13.getClassesXMethods(2);
+        else if (input==11){
+            System.out.print("Please, enter value of X : ");
+            input = sc.nextInt();
+            question8To13.getClassesXMethods(input);
         }
-        else if (x==12){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
+        else if (input==12){
             question8To13.getMethods10Lines();
         }
-        else if (x==13){
-            question8To13 = new Question8To13(question1.getTotalClassesNbr(), question3_5_7.getTotalMethodsNbr());
-            System.out.println(question8To13.getMaxParamsNbr());
+        else if (input==13){
+            question8To13.getMaxParamsNbr();
         }
-        else if (x==14){
+        else if (input==14){
+            new GUIManager().startProcess();
             System.out.println(question14.callGraph());
         }
+
+        System.out.print("\nDo you want to make another test (y/*): ");
+        String response = sc.next();
+        if (response.equals("y")) menu();
 
     }
 
