d628da5d0e8c44879bf43944daaf89caeabab2f3
amixyas <i.amirat@esi-sba.dz>
2022-10-14 17:55:23 +0200

    update qst 8 to 11 rsp

--- src/main/java/responses/Question8To13.java
+++ src/main/java/responses/Question8To13.java
@@ -3,6 +3,7 @@ package responses;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ public class Question8To13 {
     List<String> classes10methods;
     List<String> classes10attributes;
     List<String> classes10methodsAttributes =  new ArrayList<>();
-    List<String> classesXmethods =  new ArrayList<>();
+    List<String> classesXmethods;
     List<String> methods10lines;    int maxClasses;
     int maxMethods;
 
@@ -32,35 +33,37 @@ public class Question8To13 {
         if (maxMethods == 0 || (doubleNumber - maxMethods) >= 0.5 ) maxMethods += 1;
     }
 
-    public List<String> getClasses10Methods() {
+    public List<String> getClasses10Methods(boolean show) {
         classes10methods =  new ArrayList<>();
-        Map<String, MethodDeclaration[]> methods = classVisitor.getMethods();
+        Map<String, MethodDeclaration[]> methodsTemp = new HashMap<>();
+        for (var entry : classVisitor.getMethods().entrySet()) methodsTemp.put(entry.getKey(),entry.getValue());
 
         int max = 0;
         String name = "";
         for (int i = 1; i <= maxClasses; i++) {
             for (var entry : methods.entrySet()) {
-                if (max <= entry.getValue().length && !name.equals(entry.getKey()) ) {
+                if (max < entry.getValue().length) {
                     max = entry.getValue().length;
                     name = entry.getKey();
                 }
             }
             classes10methods.add(name);
             max = 0;
-            methods.remove(name);
+            methodsTemp.remove(name);
         }
-        //classes10methods.forEach(classeName-> System.out.println(classeName));
+        if (show) classes10methods.forEach(classeName-> System.out.println("-"+classeName));
         return classes10methods;
     }
 
-    public List<String> getClasses10Attirbutes() {
-        Map<String, List<String>> attributes = classVisitor.getParentVariables();
+    public List<String> getClasses10Attirbutes(boolean show) {
         classes10attributes =  new ArrayList<>();
+        Map<String, List<String>> attributesTemp = new HashMap<>();
+        for (var entry : classVisitor.getParentVariables(false).entrySet()) attributesTemp.put(entry.getKey(),entry.getValue());
 
         int max = 0;
         String name = "";
         for (int i = 1; i <= maxClasses; i++) {
-            for (var entry : attributes.entrySet()) {
+            for (var entry : attributesTemp.entrySet()) {
                 if (max < entry.getValue().size()) {
                     max = entry.getValue().size();
                     name = entry.getKey();
@@ -68,38 +71,34 @@ public class Question8To13 {
             }
             classes10attributes.add(name);
             max = 0;
-            attributes.remove(name);
+            attributesTemp.remove(name);
         }
-        //classes10attributes.forEach(classeName-> System.out.println(classeName));
-
+        if (show) classes10attributes.forEach(classeName-> System.out.println("-"+classeName));
         return classes10attributes;
     }
 
     public List<String> getClasses10methodsAttributes() {
-        getClasses10Methods();
-        getClasses10Attirbutes();
+        getClasses10Methods(false);
+        getClasses10Attirbutes(false);
         classes10methods.forEach(cm->{classes10attributes.forEach(ct->{if(cm.equals(ct))classes10methodsAttributes.add(cm);});});
-        //classes10methodsAttributes.forEach(classeName-> System.out.println(classeName));
+        classes10methodsAttributes.forEach(classeName-> System.out.println("-"+classeName));
         return classes10methodsAttributes;
     }
 
-    public List<String> getClassesXMethods(int x) {
-        for (var entry : methods.entrySet()) if (x <= entry.getValue().length) classesXmethods.add(entry.getKey());
-        //classesXmethods.forEach(classeName-> System.out.println(classeName));
-        return classesXmethods;
-    }
 
     public List<String> getMethods10Lines () {
         methods10lines =  new ArrayList<>();
-        Map<String, MethodDeclaration[]> methods = classVisitor.getMethods();
+
+        Map<String, MethodDeclaration[]> methodsTemp = new HashMap<>();
+        for (var entry : classVisitor.getMethods().entrySet()) methodsTemp.put(entry.getKey(),entry.getValue());
 
         int max = 0;
         String name = "";
         for (int i = 1; i <= maxMethods; i++) {
-            for (var entry : methods.entrySet()) {
+            for (var entry : methodsTemp.entrySet()) {
                 for (MethodDeclaration method : entry.getValue()) {
                     //System.out.println( method.getName().toString());
-                    if (max <= method.getBody().toString().split("\n").length && ! name.equals(method.getName().toString())) {
+                    if (max <= method.getBody().toString().split("\n").length) {
                         max = method.getBody().toString().split("\n").length;
                         name = method.getName().toString();
                     }
@@ -107,15 +106,21 @@ public class Question8To13 {
             }
             methods10lines.add(name);
             max = 0;
-            methods.remove(name);
+            methodsTemp.remove(name);
         }
-        //methods10lines.forEach(methodName-> System.out.println(methodName));
+        methods10lines.forEach(methodName-> System.out.println("-"+methodName));
         return methods10lines;
-        //re-init max to 0 + remove the element
+    }
+
+
+    public List<String> getClassesXMethods(int x) {
+        classesXmethods =  new ArrayList<>();
+        for (var entry : methods.entrySet()) if (x <= entry.getValue().length) classesXmethods.add(entry.getKey());
+        classesXmethods.forEach(classeName-> System.out.println("-"+classeName));
+        return classesXmethods;
     }
 
     public int getMaxParamsNbr () {
-
         int max = 0;
         for (int i = 1; i <= maxMethods; i++)
             for (var entry : methods.entrySet())
